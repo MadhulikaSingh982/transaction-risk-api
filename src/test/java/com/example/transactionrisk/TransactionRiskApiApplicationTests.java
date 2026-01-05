412fe1f5acfd2973dac85690822bfe4c794a5888
Madhulika Singh <madhulika.singh982@gmail.com>
2026-01-06 09:21:01 +1100

    Initial Commit

---
+++ src/test/java/com/example/transactionrisk/TransactionRiskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionrisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionRiskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
